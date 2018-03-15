3f2f592e8265eae12cb9be3b9c906ac0575c4b0a
Rajat Vig <rajat.vig@gmail.com>
2018-03-14 22:45:10 -0700

    Clean up layers; Use new direnv

--- direnv/packages.el
+++
@@ -1,8 +0,0 @@
-(setq direnv-packages
-      '(
-        projectile-direnv
-        ))
-
-(defun direnv/post-init-projectile-direnv ()
-  (add-hook 'projectile-mode-hook 'projectile-direnv-export-variables)
-  (add-hook 'compilation-filter-hook 'xterm-color-filter))
--- objc/packages.el
+++ objc/packages.el
@@ -6,10 +6,6 @@
 (defun objc/init-objc-font-lock ()
   (add-to-list 'auto-mode-alist '("\\.m$" . objc-mode))
   (add-to-list 'auto-mode-alist '("\\.mm$" . objc-mode))
-  (add-to-list 'auto-mode-alist '("Fastfile$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gymfile$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Scanfile$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Deliverfile$" . ruby-mode))
   (add-to-list 'magic-mode-alist
     `(,(lambda ()
          (and (string= (file-name-extension buffer-file-name) "h")
