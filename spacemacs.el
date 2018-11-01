45e64a432fb5dc43501b5726ca5576c4bfacb3f6
Rajat Vig <rajat.vig@gmail.com>
2018-10-31 23:43:46 -0700

    not show line numbers

--- spacemacs.el
+++ spacemacs.el
@@ -338,7 +338,7 @@ values."
     ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
     ;; derivatives. If set to `relative', also turns on relative line numbers.
     ;; (default nil)
-    dotspacemacs-line-numbers t
+    dotspacemacs-line-numbers nil
     ;; Code folding method. Possible values are `evil' and `origami'.
     ;; (default 'evil)
     dotspacemacs-folding-method 'evil
